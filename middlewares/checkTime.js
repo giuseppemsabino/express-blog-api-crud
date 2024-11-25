98fb98b9b01ce919316f4eb10cb6fe4e1be37dcd
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-25 17:14:39 +0100

    middleware first expamle lesson today

---
+++ middlewares/checkTime.js
@@ -0,0 +1,11 @@
+function checkTime(req,res,next){
+
+    const now = new Date();
+    const nowText = now.toLocaleDateString();
+    console.log(nowText);
+    
+
+    next();
+}
+
+module.exports = checkTime;
