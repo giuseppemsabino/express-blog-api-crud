7e1017c188515d74423af7b1f754326562eed2ca
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-09 16:15:29 +0100

    index done and conncetion success

--- controllers/postControl.js
+++ controllers/postControl.js
@@ -1,100 +1,106 @@
-const postData = require("../data/postList");
+const connection = require('../db/conn')
 
 //index
 function index(req, res) {
-  res.json(postData);
+  // res.json(postData);
 }
 
 //show
 function show(req, res) {
 
-  const id = parseInt(req.params.id);
-  
+  // const id = parseInt(req.params.id);
 
-  const postByid = postData.find((post) => post.id === id);
+  // const postByid = postData.find((post) => post.id === id);
 
-  // console.log("trova",post);
+  // // console.log("trova",post);
 
-  if (!postByid) {
-    return res.status(404).json({
-      error: "Not found",
-    });
-  }
+  // if (!postByid) {
+  //   return res.status(404).json({
+  //     error: "Not found",
+  //   });
+  // }
 
-  res.send(postByid);
+  // res.send(postByid);
+
+  const sql = 'SELECT * FROM blog.posts;'
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({error: 'Database Query Failed'});
+    res.json(results);
+  })
 }
 
 //store
 function store(req, res) {
-  const { title, content, image, tags } = req.body;
+  // const { title, content, image, tags } = req.body;
 
-  if (!title || !content || !image?.length) {
-    return res.status(400).json({ error: "invalid params" });
-  }
+  // if (!title || !content || !image?.length) {
+  //   return res.status(400).json({ error: "invalid params" });
+  // }
 
-  const newId = parseInt(postData.at(-1).id) + 1;
-  // console.log(newId);
-  newPost = {
-    id: newId,
-    title: title,
-    content: content,
-    image: image,
-    tags: tags,
-  };
-  console.log(newPost);
+  // const newId = parseInt(postData.at(-1).id) + 1;
+  // // console.log(newId);
+  // newPost = {
+  //   id: newId,
+  //   title: title,
+  //   content: content,
+  //   image: image,
+  //   tags: tags,
+  // };
+  // console.log(newPost);
 
-  postData.push(newPost);
+  // postData.push(newPost);
 
-  res.status(201).json({
-    message: "Post creato con successo!",
-    post: newPost,
-  });
+  // res.status(201).json({
+  //   message: "Post creato con successo!",
+  //   post: newPost,
+  // });
 }
 
 //update
 function update(req, res) {
-  const id = parseInt(req.params.id);
+  // const id = parseInt(req.params.id);
 
-  let postByid = postData.find((post) => post.id === id);
+  // let postByid = postData.find((post) => post.id === id);
 
-  if (!postByid) {
-    return res.status(404).json({
-      error: "Not found",
-      message: "Post non torvato",
-    });
-  }
+  // if (!postByid) {
+  //   return res.status(404).json({
+  //     error: "Not found",
+  //     message: "Post non torvato",
+  //   });
+  // }
 
-  const { title, content, image, tags } = req.body;
+  // const { title, content, image, tags } = req.body;
 
-  if (!title || !content || !image || !tags?.length) {
-    return res.status(400).json({ error: "invalid params" });
-  }
+  // if (!title || !content || !image || !tags?.length) {
+  //   return res.status(400).json({ error: "invalid params" });
+  // }
 
-  postByid.title = title;
-  postByid.content = content;
-  postByid.image = image;
-  postByid.tags = tags;
+  // postByid.title = title;
+  // postByid.content = content;
+  // postByid.image = image;
+  // postByid.tags = tags;
 
-  res.send(postByid);
+  // res.send(postByid);
 }
 
 //modify
 function modify(req, res) {
-  const id = parseInt(req.params.id);
-  res.json(`Modifica parziale del post con id ${id}`);
+  // const id = parseInt(req.params.id);
+  // res.json(`Modifica parziale del post con id ${id}`);
 }
 
 //destroy
 function destroy(req, res) {
-  const id = parseInt(req.params.id);
+  // const id = parseInt(req.params.id);
 
-  const postByid = postData.find((post) => post.id == id);
+  // const postByid = postData.find((post) => post.id == id);
 
-  const postIndex = postData.indexOf(postByid);
+  // const postIndex = postData.indexOf(postByid);
 
-  postData.splice(postIndex, 1);
+  // postData.splice(postIndex, 1);
 
-  res.json(postData);
+  // res.json(postData);
 }
 
 module.exports = { index, show, store, update, modify, destroy };
--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2");
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "pizzeria",
+  password: "password",
+  database: "blog",
 });
+
 connection.connect((err) => {
   if (err) throw err;
-  console.log("Connected to MySQL!");
+  console.log("Connected to MySQL");
 });
 module.exports = connection;
